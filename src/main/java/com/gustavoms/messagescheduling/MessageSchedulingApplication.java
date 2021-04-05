4de214bae8b4998101f3dfc75a78394bc5143b6f
Gustavo Martins de Souza <gustavoms11@gmail.com>
2021-04-05 18:44:12 -0300

    feat(): Initial commit

---
+++ src/main/java/com/gustavoms/messagescheduling/MessageSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoms.messagescheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoms/messagescheduling/MessageSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoms.messagescheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
